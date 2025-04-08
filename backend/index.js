fe6385a0f901194b780cc9cc744b81c4215901eb
kalkiG108 <garvitcoder@gmail.com>
2025-04-08 09:58:52 +0530

    Backend Setup

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({});
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    return res.status(200).json({
+        message: "hey",
+        success: true
+    })
+});
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http://localhost:5173',
+    credentials:true
+}
+
+app.use(cors(corsOptions));
+
+const PORT = 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`);
+})
