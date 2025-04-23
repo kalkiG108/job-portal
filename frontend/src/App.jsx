f614effd14954cb3ef5370263c9775e257d9f35b
kalkiG108 <garvitcoder@gmail.com>
2025-04-23 11:24:58 +0530

    Build the Navbar Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import "./App.css";
+import Navbar from "./components/shared/Navbar";
 function App() {
   return (
     <>
-      <h1 className='text-red-500'>Let's build job portal</h1>
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
