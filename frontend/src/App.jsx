10a9735fe9a4b49a4baec7132067829f0c78660a
kalkiG108 <garvitcoder@gmail.com>
2025-04-26 07:10:51 +0530

    Setup Multer for File Upload & Build Login/Signup Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,31 @@
-import "./App.css";
-import Navbar from "./components/shared/Navbar";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
+import { Toaster } from "sonner";
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
+])
 function App() {
+
   return (
-    <>
-      <Navbar />
-    </>
-  );
+    <div>
+      <RouterProvider router={appRouter} />
+      <Toaster position="bottom-right" />
+    </div>
+  )
 }
 
-export default App;
+export default App
