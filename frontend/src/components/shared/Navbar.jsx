55590f27849251a94a89b4e6d2f61aa10cafb988
kalkiG108 <garvitcoder@gmail.com>
2025-04-27 12:47:30 +0530

    Build Home Page

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -17,17 +17,17 @@ const Navbar = () => {
         </div>
         <div className="flex items-center gap-12">
           <ul className="flex font-medium items-center gap-5">
-            <li>Home</li>
-            <li>Jobs</li>
-            <li>Browse</li>
+            <li className="hover:border-b-2 hover:border-b-[#6A38C2] transition-all duration-10"><Link to="/">Home</Link></li>
+            <li className="hover:border-b-2 hover:border-b-[#6A38C2] transition-all duration-10">Jobs</li>
+            <li className="hover:border-b-2 hover:border-b-[#6A38C2] transition-all duration-10">Browse</li>
           </ul>
           {!user ? (
             <div className="flex items-center gap-2">
               <Link to="/login">
-                <Button variant="outline">Login</Button>
+                <Button variant="outline" className="cursor-pointer">Login</Button>
               </Link>
               <Link to="/signup">
-                <Button className="bg-[#6A38C2] hover:bg-[#5b30a6]">
+                <Button className="bg-[#6A38C2] cursor-pointer hover:bg-[#5b30a6]">
                   Signup
                 </Button>
               </Link>
