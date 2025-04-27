790d3fb1980623aad5dfa8844a659f7db75c5088
kalkiG108 <garvitcoder@gmail.com>
2025-04-27 18:07:53 +0530

    Build Job Description Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
 import Home from './components/Home'
 import Jobs from './components/Jobs'
+import JobDescription from './components/JobDescription'
 import Browse from './components/Browse'
 import Profile from './components/Profile'
 import { Toaster } from "sonner";
@@ -23,6 +24,10 @@ const appRouter = createBrowserRouter([
   {
     path: '/jobs',
     element: <Jobs />
+  },
+  {
+    path: '/description/:id',
+    element: <JobDescription />
   },
   {
     path: '/browse',
--- frontend/src/components/Job.jsx
+++ frontend/src/components/Job.jsx
@@ -3,10 +3,13 @@ import { Button } from "./ui/button";
 import { Bookmark } from "lucide-react";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
+import { useNavigate } from "react-router-dom";
 
-const Job = ({ job }) => {
+const Job = () => {
+  const navigate = useNavigate();
+  const jobId = "dsfnksnklngfjdnlfdlsk";
   return (
-    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100 cursor-pointer">
+    <div className="p-5 rounded-md shadow-xl bg-white border border-gray-100">
       <div className="flex items-center justify-between">
         <p className="text-sm text-gray-500">2 days ago</p>
         <Button variant="outline" className="rounded-full" size="icon">
@@ -17,7 +20,7 @@ const Job = ({ job }) => {
       <div className="flex items-center gap-2 my-2">
         <Button className="p-6" variant="outline" size="icon">
           <Avatar>
-            <AvatarImage src='' />
+            <AvatarImage src="" />
           </Avatar>
         </Button>
         <div>
@@ -42,8 +45,15 @@ const Job = ({ job }) => {
         </Badge>
       </div>
       <div className="flex items-center gap-4 mt-4">
-        <Button variant="outline">Details</Button>
-        <Button className="bg-[#7209b7]">Save For Later</Button>
+        <Button
+          onClick={() => navigate(`/description/${jobId}`)}
+          variant="outline"
+          className="cursor-pointer"
+        >
+          Details
+        </Button>
+
+        <Button className="bg-[#7209b7] cursor-pointer">Save For Later</Button>
       </div>
     </div>
   );
